2b26391ee538581fc8c18e8e2266fe9ecb63b8a9
Lanka Hsu <lankahsu@gmail.com>
2022-09-26 11:45:32 +0800

    update user/library.mk

--- user/library.mk
+++ user/library.mk
@@ -1,21 +1,3 @@
-ifeq ($(PJ_HAS_PHILIO_SDK_V2),y)
-LIBS_yes += -lsdk -lapp
-endif
-
-ifeq ("$(PJ_HAS_LOGCAT)", "yes")
-LIBS_yes += -llogcat
-ifneq (,$(wildcard $(SDK_INC_DIR)/logcat_api.h))
-CFLAGS += -include $(SDK_INC_DIR)/logcat_api.h
-endif
-endif
-
-ifeq ("$(PJ_HAS_TIMEZONE)", "yes")
-LIBS_yes += -ltimezone
-ifneq ("$(wildcard $(SDK_INC_DIR)/timezone_api.h)","")
-CFLAGS += -include $(SDK_INC_DIR)/timezone_api.h
-endif
-endif
-
 include $(PJ_MK_USER_LIB_MCTT)
 include $(PJ_MK_USER_LIB_PUB)
 
